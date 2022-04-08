ba4d73925f812e24c9cda92c361dbc3899f83216
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-04-08 14:57:32 +0900

    feat: URIにバージョンを追加

--- src/libs/router.ts
+++ src/libs/router.ts
@@ -1,12 +1,14 @@
 import { Router } from "../deps.ts";
 import { artworkController } from "../controllers/artworks_controller.ts";
 
+const v1 = new Router();
+
+v1.get("/cd/:id", artworkController.get);
+v1.get("/list", artworkController.search);
+
 const router = new Router();
 
 router.get("/", artworkController.redirect);
-
-router.get("/artwork/:id", artworkController.get);
-
-router.get("/list", artworkController.search);
+router.get("/v1", v1.routes(), v1.allowedMethods());
 
 export { router };
