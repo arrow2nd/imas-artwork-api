270d92ed2c5b69f0828a7157e56508ec99d583d1
arrow2nd <arrow2nd@users.noreply.github.com>
2022-07-29 15:26:01 +0000

    chore(deps): 依存関係を更新

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { serve, Status } from "https://deno.land/std@0.149.0/http/mod.ts";
+export { serve, Status } from "https://deno.land/std@0.150.0/http/mod.ts";
 
 import { Context, Hono } from "https://deno.land/x/hono@v2.0.5/mod.ts";
 export { Hono };
