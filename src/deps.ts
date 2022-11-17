a5527f683cc5d0e375adc05f4cb0e34278ddefbc
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-11-17 21:03:02 +0900

    feat npm import (#55)
    * chore(deps): kyをnpm経由でインポートするよう変更
    
    * refactor: ライブラリのexport周りを整理

--- src/deps.ts
+++ src/deps.ts
@@ -1,12 +1,8 @@
 export { serve, Status } from "https://deno.land/std@0.164.0/http/mod.ts";
 
-import { Context, Hono } from "https://deno.land/x/hono@v2.5.1/mod.ts";
-export { Hono };
-export type { Context };
+export { type Context, Hono } from "https://deno.land/x/hono@v2.5.1/mod.ts";
 
-import {
-  Document,
+export {
+  type Document,
   MongoClient,
 } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-export { MongoClient };
-export type { Document };
--- src/tools/lantis/main.ts
+++ src/tools/lantis/main.ts
@@ -1,4 +1,3 @@
-import { Document } from "../../dev_deps.ts";
 import { fetchHtml } from "../../libs/fetch.ts";
 import { wait } from "../../libs/util.ts";
 
@@ -26,10 +25,8 @@ export async function updateLantis(ids: string[]) {
     // 詳細ページへのリンクを取得
     const detailPages = res.doc
       .getElementsByClassName("dsc_box")
-      .map((e: Document) =>
-        e.getElementsByTagName("a").map((e: Document) =>
-          e.getAttribute("href") || ""
-        )
+      .map((e) =>
+        e.getElementsByTagName("a").map((e) => e.getAttribute("href") || "")
       )
       .flat();
 
