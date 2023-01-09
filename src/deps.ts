487f76e79fe471fa10c5a715d0a2a122f723bcc3
denopendabot <denopendabot@github.com>
2023-01-09 15:17:32 +0000

    build(deps): bump hono from v2.7.1 to v2.7.2

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 export { serve, Status } from "https://deno.land/std@0.171.0/http/mod.ts";
 
-export { type Context, Hono } from "https://deno.land/x/hono@v2.7.1/mod.ts";
+export { type Context, Hono } from "https://deno.land/x/hono@v2.7.2/mod.ts";
 
 export {
   type Document,
