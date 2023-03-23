a18e08ee6edf8ddefca1a985d76aa2532fa9cfe6
denopendabot <denopendabot@github.com>
2023-03-23 00:34:41 +0000

    build(deps): bump std from 0.180.0 to 0.181.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { serve, Status } from "https://deno.land/std@0.180.0/http/mod.ts";
+export { serve, Status } from "https://deno.land/std@0.181.0/http/mod.ts";
 
 export { type Context, Hono } from "https://deno.land/x/hono@v3.1.2/mod.ts";
 
