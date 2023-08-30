d207f0d74dcbda3168bea80fe25e079a83c27280
denopendabot <denopendabot@github.com>
2023-08-30 00:29:09 +0000

    build(deps): bump hono from v3.5.5 to v3.5.6

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 export { serve, Status } from "https://deno.land/std@0.200.0/http/mod.ts";
 
-export { type Context, Hono } from "https://deno.land/x/hono@v3.5.5/mod.ts";
+export { type Context, Hono } from "https://deno.land/x/hono@v3.5.6/mod.ts";
 
 export {
   type Document,
