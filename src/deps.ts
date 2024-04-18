959e5c5d3c20fc5274461701ce8a96db512d791d
denopendabot <denopendabot@github.com>
2024-04-18 00:03:06 +0000

    build(deps): bump mongo from v0.32.0 to v0.33.0

--- src/deps.ts
+++ src/deps.ts
@@ -3,4 +3,4 @@ export { type Context, Hono } from "https://deno.land/x/hono@v4.2.4/mod.ts";
 export {
   type Document,
   MongoClient,
-} from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+} from "https://deno.land/x/mongo@v0.33.0/mod.ts";
